dbf8b3abae2967d7226af7c12992d8546c34e3fd
Thomas Jakobi <thomas.jakobi@partout.info>
2020-02-28 22:21:15 +0100

    Update to 2.0.9-pl
    - Fixed: Removed modAction suppport
    - Changed: Removed standard build script (GPM build script is available)

--- assets/components/redirector/connector.php
+++ assets/components/redirector/connector.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * Redirector Connector
+ * Redirector connector
  *
  * @package redirector
+ * @subpackage connector
+ *
+ * @var modX $modx
  */
-require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
-require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
-require_once MODX_CONNECTORS_PATH.'index.php';
 
-$corePath = $modx->getOption('redirector.core_path',null,$modx->getOption('core_path').'components/redirector/');
-require_once $corePath.'model/redirector/redirector.class.php';
-$modx->redirector = new Redirector($modx);
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
+require_once MODX_CONNECTORS_PATH . 'index.php';
 
-$modx->lexicon->load('redirector:default');
+$corePath = $modx->getOption('redirector.core_path', null, $modx->getOption('core_path') . 'components/redirector/');
+/** @var Redirector $redirector */
+$redirector = $modx->getService('redirector', 'Redirector', $corePath . 'model/redirector/', array(
+    'core_path' => $corePath
+));
 
 /* handle request */
-$path = $modx->getOption('processorsPath',$modx->redirector->config,$corePath.'processors/');
+$path = $modx->getOption('processorsPath', $modx->redirector->config, $corePath . 'processors/');
 $modx->request->handleRequest(array(
     'processors_path' => $path,
     'location' => '',
